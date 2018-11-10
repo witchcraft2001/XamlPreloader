0716712a4cbbb69d07296b7f70c051c165551495
Dmitry Mikhaltchenkov <mikhaltchenkov@gmail.com>
2018-11-10 10:57:31 +0300

    + added BouncingCirclesPreloader

---
+++ XamlPreloader/BouncingCirclesPreloader.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace XamlPreloader
+{
+    /// <summary>
+    /// Логика взаимодействия для Preloader.xaml
+    /// </summary>
+    public partial class BouncingCirclesPreloader : UserControl
+    {
+        public BouncingCirclesPreloader()
+        {
+            InitializeComponent();
+        }
+    }
+}
